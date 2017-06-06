a8bfb896e277f7577f533096ca36532de97eaf62
benrhine <rhine.ben@gmail.com>
2017-06-06 14:00:01 -0400

    testing

--- complete/client/src/security/auth.js
+++ complete/client/src/security/auth.js
@@ -1,18 +1,6 @@
 export default {
-    logIn(auth) {
-        console.log(`Auth:login: ${auth}`);
-        if (auth) {
-            localStorage.auth = JSON.stringify(auth);
-        }
-    },
 
-    logOut() {
-        console.log(`Auth:logOut: ${localStorage.auth}`);
-        delete localStorage.auth;
-    },
-
-    loggedIn() {
-        console.log(`Auth:loggedIn?: ${localStorage.auth ? 'true' : 'false'}`);
-        return !!localStorage.auth;
-    }
-};
+  loggedIn() {
+    return true
+  }  
+}
