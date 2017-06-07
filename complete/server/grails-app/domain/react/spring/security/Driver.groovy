a3c9c5a228992829fa0603f92728626e72bff556
Zachary Klein <zak@silver-chalice.com>
2017-06-07 11:52:38 -0500

    Secured the domain resources

--- complete/server/grails-app/domain/react/spring/security/Driver.groovy
+++ complete/server/grails-app/domain/react/spring/security/Driver.groovy
@@ -1,7 +1,9 @@
 package react.spring.security
 
+import grails.plugin.springsecurity.annotation.Secured
 import grails.rest.Resource
 
+@Secured(['ROLE_DRIVER'])
 @Resource(uri = '/api/driver')
 class Driver extends User {
 
