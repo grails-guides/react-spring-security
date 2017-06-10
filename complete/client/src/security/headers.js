2f6a61dc3dafc86f37771529e0c63477481527af
Zachary Klein <zak@silver-chalice.com>
2017-06-10 01:42:05 -0500

    Added headers.js

---
+++ complete/client/src/security/headers.js
@@ -0,0 +1,6 @@
+export default () => {
+  return {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.auth ? JSON.parse(localStorage.auth).access_token : null}`
+  }
+}
