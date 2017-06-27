f6a458f58e6c4ccd39141c0a60addd64acaadfcb
Zachary Klein <zak@silver-chalice.com>
2017-06-27 00:35:33 -0500

    Added refresh token implementation/docs

--- complete/client/src/security/auth.js
+++ complete/client/src/security/auth.js
@@ -2,6 +2,7 @@ import {SERVER_URL} from './../config';
 import {checkResponseStatus} from './../handlers/responseHandlers';
 import headers from './../security/headers';
 import 'whatwg-fetch';
+import qs from 'qs';
 
 //<1>
 export default {
@@ -13,12 +14,30 @@ export default {
     delete localStorage.auth;
   },
 
-  loggedIn() {  //<4>
+  refreshToken() { //<4>
+    return fetch(
+      `${SERVER_URL}/oauth/access_token`,
+      { method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: qs.stringify({
+          grant_type: 'refresh_token',
+          refresh_token: JSON.parse(localStorage.auth).refresh_token
+        })
+      })
+      .then(checkResponseStatus)
+      .then((a) => localStorage.auth = JSON.stringify(a))
+      .catch(() => { throw new Error("Unable to refresh!")})
+  },
+
+  loggedIn() {  //<5>
     return localStorage.auth && fetch(
-        `${SERVER_URL}/api/vehicle`, //<5>
+        `${SERVER_URL}/api/vehicle`, //<6>
         {headers: headers()})
         .then(checkResponseStatus)
         .then(() => { return true })
+        .catch(this.refreshToken)
         .catch(() => { return false });
   }
 };
